fd6321fabee0a723cf0eda81454e3c91418824de
Nikko Baez <nikkobaez@Nikkos-MacBook-Air.local>
2023-02-04 18:42:54 -0600

    added flatlist and slide component

---
+++ components/Slide.js
@@ -0,0 +1,55 @@
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+
+const { width, height } = Dimensions.get('screen')
+
+export default function Slide({item}) {
+    return (
+        <View style={styles.container}>
+            <Image 
+                source={item.image}
+                resizeMode="contain"
+            />
+            <View style={styles.content}> 
+                <Text style={styles.title}>
+                    {item.title}
+                    <Text style={styles.titleColored}> {item.titleColored} </Text>
+                </Text>
+                <Text style={styles.subtitle}>
+                    {item.subtitle}
+                </Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: width,
+        height: height * 0.60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: 'red',
+        borderWidth: 1,
+    },
+    content: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontFamily: 'Poppins_700Bold',
+        textAlign: 'center',
+        width: 300,
+        marginTop: 40,
+    },
+    titleColored: {
+        color: '#4b8e4b',
+    },
+    subtitle: {
+        fontSize: 15,
+        fontFamily: 'Poppins_400Regular',
+        textAlign: 'center',
+        width: 325,
+        marginTop: 15,
+    }
+  });
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,18 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Slide from '../components/Slide';
+
+const GREETING_DATA = [
+    {
+        id: 0,
+        image: require('../assets/peace-lily-plant.png'),
+        title: 'The best collection of' ,
+        titleColored: 'indoor & outdoor plants',
+        subtitle: 'We have a collection of the finest plants, each selected for their beauty and vitality.'
+    },
+    {
+        id: 1,
+        image: require('../assets/calathea-medallion-plant.png'),
+        title: 'Keep all of your plants',
+        titleColored: 'alive, healthy, & strong',
+        subtitle: 'We ensure the longevity of your plants by providing them the very best of care.'
+    },
+    {
+        id: 2,
+        image: require('../assets/monstera-deliciosa-plant.png'),
+        title: 'Plant a tree & together',
+        titleColored: ' lets green up the earth',
+        subtitle: 'Planting a tree helps our environment and creates a much cleaner atmosphere.'
+    },
+];
+
+const { width, height } = Dimensions.get('screen');
 
 export default function WelcomeScreen() {
-  return (
-    <View>
-        <Text> Hi! </Text>
-    </View>
+
+    return (
+        <ScrollView style={styles.container}>
+            <FlatList 
+                data={GREETING_DATA}
+                renderItem={({item}) => <Slide item={item}/>}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                pagingEnabled={true}
+                snapToAlignment="center"
+            />
+        </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: height,
+    width: width,
   },
 });
