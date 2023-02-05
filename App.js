94d5e30d0312a5c1acd6e6b15cbf7ac596653428
Nikko Baez <nikkobaez@Nikkos-MacBook-Air.local>
2023-02-04 18:05:38 -0600

    installed and added poppins expo font

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './screens/WelcomeScreen';
+import AppLoading from 'expo-app-loading';
+import { 
+  useFonts, 
+  Poppins_400Regular, 
+  Poppins_500Medium, 
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <WelcomeScreen />
-    </SafeAreaView>
-  );
-}
+
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    <AppLoading />
+  } else {
+    return (
+      <SafeAreaView>
+        <WelcomeScreen />
+      </SafeAreaView>
+    );
+  }
+} 
 
 const styles = StyleSheet.create({
 
